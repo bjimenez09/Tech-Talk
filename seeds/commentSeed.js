c5482dbadca1016c657e4f8628a6000a30718282
Brandon Jimenez <bjimenezp09@gmail.com>
2023-02-01 16:21:00 -0600

    reformatted main

--- seeds/commentSeed.js
+++ seeds/commentSeed.js
@@ -1,20 +1,12 @@
 const { Comment } = require('../models');
 
-const commentData = [{
-  comment_text: 'Lorem ipsum dolor sit amet',
-  user_id: 1,
-  post_id: 1
-},
-{
-  comment_text: 'consectetur adipiscing elit',
-  user_id: 2,
-  post_id: 2
-},
-{
-  comment_text: 'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
-  user_id: 3,
-  post_id: 3
-}
+const commentData = [
+  {
+    comment_text: 'DM me for more information',
+    user_id: 1,
+    post_id: 1,
+
+  }
 ];
 
 const seedComments = () => Comment.bulkCreate(commentData);
