c5fa6388f3720a28896d92ac3fc8001383f75923
Brandon Jimenez <bjimenezp09@gmail.com>
2023-02-01 15:59:33 -0600

    fixed controllers and renamed files

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
 
-const dashboardRoutes = require('./dashboard-routes.js');
-const homeRoutes = require('./home-routes');
 const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+const dashboardRoutes = require('./dashboardRoutes');
 
 router.use('/', homeRoutes);
-router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
+router.use('/dashboard', dashboardRoutes);
+router.use((req, res) => {
+  res.status(404).end();
+});
 
 module.exports = router;
